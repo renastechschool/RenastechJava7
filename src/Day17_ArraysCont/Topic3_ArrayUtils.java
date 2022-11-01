50195c0d80fbc5e03195bf3cf73e81d7db413be8
zyilmaz02 <zyylmz24@gmail.com>
2022-10-31 21:48:26 -0500

    Day 17

---
+++ src/Day17_ArraysCont/Topic3_ArrayUtils.java
@@ -0,0 +1,28 @@
+package Day17_ArraysCont;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Topic3_ArrayUtils {
+
+    public static void main(String[] args) {
+        int [] numbers = {78,34,12,31,45};
+        //Arrays.toString(numbers);
+        System.out.println("All the elements in Array: "+Arrays.toString(numbers));
+
+
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++++");
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(" number: "+numbers[i]);
+        }
+        System.out.println("++++++++++++++++++++++++++++++++++++++++++");
+        String [] nms = {"Ali", "John","Rami"};
+
+        String nms1 = Arrays.toString(nms);
+        System.out.println("This is string rep of nms Array: "+nms1);
+
+
+
+    }
+}
