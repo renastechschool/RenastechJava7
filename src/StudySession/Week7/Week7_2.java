9849f0e61c03e4b8e75cffc1cf0f729bc550d865
Taylan <taylantonka@gmail.com>
2022-11-03 18:52:39 -0400

    session 3s

--- src/StudySession/Week7/Week7_2.java
+++ src/StudySession/Week7/Week7_2.java
@@ -1,5 +1,7 @@
 package StudySession.Week7;
 
+import java.util.Arrays;
+
 public class Week7_2 {
     public static void main(String[] args) {
 
@@ -24,6 +26,58 @@ public class Week7_2 {
 
         System.out.println("maxNumber = " + maxNumber);
 
+        int[][] myNumbers2 = {{5,8,2,6},{4,2,1}};
+        int lengthForOneDimension = 0;
+
+
+        for(int i=0; i<myNumbers2.length;i++)
+        {
+            lengthForOneDimension += myNumbers2[i].length;
+
+
+        }
+        int[] oneDimensiononeDimension = new int[lengthForOneDimension];
+        String allElements ="";
+
+
+        for(int i=0; i<myNumbers2.length;i++)
+        {
+            for(int j=0;j<myNumbers2[i].length;j++)
+            {
+                allElements += myNumbers2[i][j]+",";
+            }
+        }
+
+        System.out.println("allElements = " + allElements);
+        String[] StringOneDimension = allElements.split(",");
+        System.out.println("StringOneDimension = " + Arrays.toString(StringOneDimension));
+        int count = 0;
+        for(String element : StringOneDimension)
+        {
+            oneDimensiononeDimension[count] = Integer.parseInt(element);
+
+            count++;
+        }
+
+        System.out.println(Arrays.toString(oneDimensiononeDimension));
+
+        //If the length is odd then return center element of the array or if the length is even then return the average of center elements
+        int[] example = {5,1,61,72,11,8,9,1};
+
+        if(example.length % 2 != 0)
+        {
+            System.out.println(example[example.length/2]);
+        }
+        else
+        {
+            System.out.println((example[example.length/2 -1] +  example[example.length/2 ])  /2 );
+        }
+
+
+
+
+
 
     }
+
 }
