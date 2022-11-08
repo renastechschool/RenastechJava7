8cd0da9218b22a4f19f469400571157d97c912be
ramazanozersf <brsheres@gmail.com>
2022-11-07 16:39:09 -0800

    create file

---
+++ src/Day21_MethodsAndMethodOverload/MethodInterview.java
@@ -0,0 +1,16 @@
+package Day21_MethodsAndMethodOverload;
+
+public class MethodInterview {
+
+    // Create a Method will print your name
+
+    public static void main(String[] args) {
+        System.out.println("Baris");
+    }
+
+    public static String myName(){
+        return "Baris";
+    }
+
+
+}
