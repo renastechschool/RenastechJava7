f9ad330487d935a533c8650387370b3919284e83
Hayri Aslan <hayriaslan88@gmail.com>
2022-11-14 20:22:50 -0500

    Day 23

---
+++ src/Day23_MethodsOverloadWrapperRecap/Topic2_CheckPrimeNumbers.java
@@ -0,0 +1,35 @@
+package Day23_MethodsOverloadWrapperRecap;
+
+public class Topic2_CheckPrimeNumbers {
+    public static void main(String[] args) {
+
+       int num = 15;
+        boolean IsItPrimeNumber = true;
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                IsItPrimeNumber = false;
+                break;
+            }
+        }
+
+        if (IsItPrimeNumber == true){
+            System.out.println("It is a prime number");
+        }
+        else {
+            System.out.println("It is NOT a prime number");
+        }
+
+    }
+
+ /*   public static boolean CheckPrimeNumbers(int param1){
+        boolean IsItPrimeNumber = true;
+
+        for (int i = 2; i < param1; i++) {
+            if ((param1 % i == 0) ){
+                IsItPrimeNumber = false;
+            }
+        }
+        return IsItPrimeNumber;
+    }
+  */
+}
