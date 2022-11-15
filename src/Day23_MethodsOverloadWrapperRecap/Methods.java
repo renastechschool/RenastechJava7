f0399cd0e6d874bcd17a613038acf9cdf9983fd9
Hayri Aslan <hayriaslan88@gmail.com>
2022-11-14 19:39:26 -0500

    Day 23

---
+++ src/Day23_MethodsOverloadWrapperRecap/Methods.java
@@ -0,0 +1,51 @@
+package Day23_MethodsOverloadWrapperRecap;
+
+public class Methods {
+    public static void main(String[] args) {
+        /*
+            Define 2 numbers and print sum of them
+
+            define a string variable and print lowercase of it.
+
+            define 2 numbers and print numbers from 1 to sum of them.
+
+            define another string and print lowercase of it
+         */
+
+        int a = 5;
+        int b = 6;
+
+      //  int sum = a+b;
+        int sum = Add(a,b);
+        System.out.println(sum);
+
+//        String s1 = "RenasTech";
+//        System.out.println(s1.toUpperCase());
+        PrintLowerCase("RenasTECH");
+
+        int c = 7;
+        int d = 3;
+
+//        int sum2 = c + d;
+        int sum2 = Add(c,d);
+        for (int i = 1; i <= sum2 ; i++) {
+            System.out.println(i);
+        }
+
+//        String s2 = "WhatEVer";
+//        System.out.println(s2.toLowerCase());
+        PrintLowerCase("WhatEVer");
+
+
+    }
+
+    public static int Add(int param1, int param2){
+        return param1 * param2;
+    }
+
+    public static void PrintLowerCase(String param1){
+        System.out.println(param1.toUpperCase());
+    }
+
+
+}
