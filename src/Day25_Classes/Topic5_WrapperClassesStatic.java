ed389ad648da988464673c5b92760cf06bd7f6dc
Hayri Aslan <hayriaslan88@gmail.com>
2022-11-16 21:40:41 -0500

    Day 25

---
+++ src/Day25_Classes/Topic5_WrapperClassesStatic.java
@@ -0,0 +1,17 @@
+package Day25_Classes;
+
+public class Topic5_WrapperClassesStatic {
+    public static void main(String[] args) {
+        /*
+        // We created a new Integer wrapper class with the value of 5
+        Integer MyInt = new Integer(5);
+
+        // We convert our Integer(MyInt) to string
+        String MyString = MyInt.toString();
+
+         */
+
+        // To convert our String to Integer, We used parseInt method of Integer.
+        Integer.parseInt("5");
+    }
+}
