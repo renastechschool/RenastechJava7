a7dedc302caa36866b5d648d4c1e5d6d28f31cd1
Taylan <taylantonka@gmail.com>
2022-11-21 19:59:04 -0500

    SSweek9_3

---
+++ src/StudySession/Week9/Example_for_Static.java
@@ -0,0 +1,10 @@
+package StudySession.Week9;
+
+public class Example_for_Static {
+
+    public static void main(String[] args) {
+
+        System.out.println(City.add(2, 2));
+        System.out.println(City.cityOfState);
+    }
+}
