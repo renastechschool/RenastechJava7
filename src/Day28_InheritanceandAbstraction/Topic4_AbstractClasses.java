0ae0017cf828062132743f851267f42a4df7c23e
Hayri Aslan <hayriaslan88@gmail.com>
2022-11-22 21:41:10 -0500

    Day 28

--- src/Day28_InheritanceandAbstraction/Topic4_AbstractClasses.java
+++ src/Day28_InheritanceandAbstraction/Topic4_AbstractClasses.java
@@ -9,6 +9,25 @@ public class Topic4_AbstractClasses {
 }
 
 /*
+Abstraction can be achieved with either abstract classes or interfaces.
+Abstraction is the process of separating ideas from their action.
+Interfaces are like skeletons. If you want to build a human, you should use that skeleton.
+Abstract classes are like skeletons, but with some meat on them as well. It's just there to make your work easier.
+You can consider an abstract class to be an interface, which already has some implementation.
+
+You do abstraction when deciding what to implement.
+You do encapsulation when hiding something that you have implemented.
+
+Interfaces simply create a kind of structure for your classes to denote that your class should have a defined and agree-upon structure.
+For example, all animals eat and make sounds. Thus you can have an interface,
+called IAnimal which only mentions that classes should Eat, and MakeSound.
+
+Abstract class: is a restricted class that cannot be used to create objects
+(to access it, it must be inherited from another class).
+Abstract method: can only be used in an abstract class, and it does not have a body.
+ The body is provided by the subclass (inherited from).
+
+ An abstract class can have both abstract and regular methods.
 
  */
 
