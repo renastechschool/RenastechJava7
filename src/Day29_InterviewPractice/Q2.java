f3294007c5e544d066326cd663cd39a7b27cb30e
ramazanozersf <brsheres@gmail.com>
2022-11-23 17:57:53 -0800

    Q2 Answer

--- src/Day29_InterviewPractice/Q2.java
+++ src/Day29_InterviewPractice/Q2.java
@@ -12,4 +12,36 @@ public class Q2 {
     // Example: Poem =>  "and the garden diminishes: grape leaves rumpled and rusty, zucchini felled by borers, tomatoes sparse on the vines"
     // Return => felled
 
+    // question, is there only one space, limitation,
+
+    public static String mostPoeticalWord(String poem){
+
+        String[] words = poem.split(" "); // ['and','the','garden',.....]
+
+        int count2 = 0;
+        String wordResult="";
+        for (int i = 0; i < words.length; i++){
+
+            String word = words[i]; // and
+
+            int count = 0;
+
+            char[] letters = word.toCharArray(); // ['a','n','d']
+            for(int j=0; j< letters.length; j++){
+                if(letters[j] == 'a' || letters[j] == 'b' || letters[j] == 'c' || letters[j] == 'd' || letters[j] == 'e'|| letters[j] == 'f' ){
+                    count=count+1;
+                }else if(letters[j] == 'k'){
+                    count=count-1;
+                }else if(letters[j] == 'x'){
+                    count=count-2;
+                }
+            }
+            if(count>count2){
+                count2 = count;
+                wordResult = words[i];
+            }
+        }
+        return wordResult;
+    }
+
 }
