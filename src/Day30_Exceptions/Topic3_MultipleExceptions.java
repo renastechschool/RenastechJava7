d4107484a18f836a82a4cba90f3665d2cdd8277f
Hayri Aslan <hayriaslan88@gmail.com>
2022-11-28 19:47:26 -0500

    Day 30

--- src/Day30_Exceptions/Topic3_MultipleExceptions.java
+++ src/Day30_Exceptions/Topic3_MultipleExceptions.java
@@ -2,6 +2,13 @@ package Day30_Exceptions;
 
 public class Topic3_MultipleExceptions {
     public static void main(String[] args) {
-
+        try {
+            int[] AnArray = {11,22,33};
+            int MyValue = AnArray[6];
+            System.out.println(MyValue);
+        }catch (Exception MyException){
+            System.out.println("We don't have a value for index 6 ");
+        }
+        System.out.println("Rest of the code");
     }
 }
---
+++ src/Day30_Exceptions/Topic4_CheckedExceptions.java
@@ -0,0 +1,7 @@
+package Day30_Exceptions;
+
+public class Topic4_CheckedExceptions {
+    public static void main(String[] args) {
+     //   Class.forName("Something");
+    }
+}
