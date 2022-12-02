d48d99de95309f3707736ce1cfadb3276c10aa7f
Hayri Aslan <hayriaslan88@gmail.com>
2022-12-01 20:16:49 -0500

    Day 32

--- src/Day32_Maps/Topic3_TreeMap.java
+++ src/Day32_Maps/Topic3_TreeMap.java
@@ -12,5 +12,25 @@ public class Topic3_TreeMap {
         ModelYearOfCars.put("Subaru", 2009);
         System.out.println(ModelYearOfCars);
 
+
+
+        System.out.println(ModelYearOfCars.keySet());
+
+        System.out.println("*****************");
+
+        for (String MyKey: ModelYearOfCars.keySet()) {
+            System.out.println("Key : "+MyKey);
+//            get(Key) - Returns the value associated with the specified key.
+            System.out.println("Value : "+ModelYearOfCars.get(MyKey));
+        }
+
+        System.out.println("*****************");
+        System.out.println( ModelYearOfCars.get("BMW"));
+        System.out.println(ModelYearOfCars.getOrDefault("BMW", -1 ));
+
+//        put(Key, Value) - Inserts the association of a key and a value into the map.
+//                If the key is already present, the new value replaces the old value.
+        ModelYearOfCars.put("Tesla", 2022);
+
     }
 }
---
+++ src/Day32_Maps/Topic4_Example1.java
@@ -0,0 +1,7 @@
+package Day32_Maps;
+
+public class Topic4_Example1 {
+    public static void main(String[] args) {
+
+    }
+}
