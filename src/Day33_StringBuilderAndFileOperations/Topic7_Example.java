c5824bbb3c5c5d5c2e28e5e492e8dc0b787efd68
Hayri Aslan <hayriaslan88@gmail.com>
2022-12-02 21:45:55 -0500

    Day 33

--- src/Day33_StringBuilderAndFileOperations/Topic7_Example.java
+++ src/Day33_StringBuilderAndFileOperations/Topic7_Example.java
@@ -4,12 +4,18 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Topic7_Example {
     public static void main(String[] args) {
         try{
             FileReader FR = new FileReader("C:/Users/Hayri/Desktop/Day33/top250.txt");
             BufferedReader MyReader = new BufferedReader(FR);
+
+            ArrayList<Movie> Top250 = new ArrayList<>();
+
+
 
             String line = MyReader.readLine();
 
@@ -34,9 +40,27 @@ public class Topic7_Example {
 
                 //(1994)
 
-                System.out.println(SB);
+                String Year = SB.substring(1, 5);
+                System.out.println("Year : "+Year);
+
+                //Another way to get year
+                //int Position2 = SB.indexOf(")");
+                //String Year = SB.substring(1, Position2);
+
+                Movie aMovie = new Movie();
+                aMovie.MovieName = Movie;
+                aMovie.Rating = Rating;
+                aMovie.Year = Year;
+
+                Top250.add(aMovie);
+
+//                System.out.println(SB);
                 line = MyReader.readLine();
             }
+
+            System.out.println(Top250.size());
+            System.out.println("*********************");
+            PrintOldMovies(Top250);
 
 
         } catch (FileNotFoundException e){
@@ -45,4 +69,26 @@ public class Topic7_Example {
             System.out.println("Error while reading file");
         }
     }
+
+    ///please create a method that takes an arraylist of movie as parameter and prints movies that older than 1999
+    ///method name = PrintOldMovies
+    /// return type  = void
+    // publicorprivate staticornot returntype methodname(parameter1type parameter1name, ........)
+    public static void PrintOldMovies(ArrayList<Movie> param1){
+        // prints movies that older than 1999
+        for (Movie MyMovieInTheList:param1) {
+            if(Integer.parseInt(MyMovieInTheList.Year) < 1999 ){
+                System.out.println(MyMovieInTheList.MovieName);
+            }
+
+        }
+    }
+
+
+}
+
+class Movie {
+    String Year;
+    String MovieName;
+    String Rating;
 }
